a1eab5abf89486e6655e93d8f6f86f0763d9422e
rockeet <rockeet@gmail.com>
2024-12-15 12:48:22 +0800

    rockside:MergeTables: create target cf; remove IngestExternalFileArg::override_cf_id

--- include/rocksdb/options.h
+++ include/rocksdb/options.h
@@ -2102,8 +2102,6 @@ struct IngestExternalFileOptions {
 
   // ToplingDB: sync file can be optional
   bool sync_file = true;
-
-  uint32_t override_cf_id = INT32_MAX;
 };
 
 ROCKSDB_ENUM_PLAIN(TraceFilterType, uint64_t,
