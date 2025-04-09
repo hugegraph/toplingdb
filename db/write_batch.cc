ea11322f14e0fabcdcc2235cc8aeddbccfba5752
rockeet <rockeet@gmail.com>
2025-04-09 19:19:50 +0800

    Revert "WriteBatch::Clear(): free memory if cap > 512K"
    This reverts commit 3a317f5631580eae91eb94905d94b87b1f3c991b.
    
    The reverted commit is less useful, it is caller's resposibility
    to manage WriteBatch properly.

--- db/write_batch.cc
+++ db/write_batch.cc
@@ -250,9 +250,6 @@ void WriteBatch::Handler::LogData(const Slice& /*blob*/) {
 bool WriteBatch::Handler::Continue() { return true; }
 
 void WriteBatch::Clear() {
-  if (rep_.capacity() > 512*1024) {
-    std::string().swap(rep_); // free memory
-  }
   rep_.clear();
   rep_.resize(WriteBatchInternal::kHeader);
 
