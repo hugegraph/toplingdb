5d3af48eeff77e5381251c5d1edaa911f613b399
rockeet <rockeet@gmail.com>
2025-04-30 21:51:00 +0800

    PessimisticTransaction::TryLock: avoid copy Status obj

--- utilities/transactions/pessimistic_transaction.cc
+++ utilities/transactions/pessimistic_transaction.cc
@@ -977,9 +977,8 @@ Status PessimisticTransaction::TryLock(ColumnFamilyHandle* column_family,
                                        bool exclusive, const bool do_validate,
                                        const bool assume_tracked) {
   assert(!assume_tracked || !do_validate);
-  Status s;
   if (UNLIKELY(skip_concurrency_control_)) {
-    return s;
+    return Status::OK();
   }
   const ColumnFamilyHandle* const cfh =
       column_family ? column_family : db_impl_->DefaultColumnFamily();
@@ -1002,9 +1001,9 @@ Status PessimisticTransaction::TryLock(ColumnFamilyHandle* column_family,
 
   // Lock this key if this transactions hasn't already locked it or we require
   // an upgrade.
-  if (!previously_locked || lock_upgrade) {
-    s = txn_db_impl_->TryLock(this, cfh_id, key, key_hash, exclusive);
-  }
+  Status s = (!previously_locked || lock_upgrade)
+           ? txn_db_impl_->TryLock(this, cfh_id, key, key_hash, exclusive)
+           : Status::OK();
 
 #if defined(TOPLINGDB_WITH_TIMESTAMP)
   const Comparator* const ucmp = cfh->GetComparator();
@@ -1049,9 +1048,10 @@ Status PessimisticTransaction::TryLock(ColumnFamilyHandle* column_family,
     // since the snapshot.  This must be done after we locked the key.
     // If we already have validated an earilier snapshot it must has been
     // reflected in tracked_at_seq and ValidateSnapshot will return OK.
-    s = ValidateSnapshot(column_family, key, &tracked_at_seq);
+    Status s2 = ValidateSnapshot(column_family, key, &tracked_at_seq);
 
-    if (!s.ok()) {
+    if (UNLIKELY(!s2.ok())) {
+      s = s2; // unlikely branch copy has no significant impact
       // Failed to validate key
       // Unlock key we just locked
       if (lock_upgrade) {
